92177ced90f082bfda59911feb7a77c11b6b8e6d
ptrierweiler <ptrierweiler@gmail.com>
2016-10-24 22:10:45 -0400

    first commit

---
+++ cpc_glb_dly_prec_download.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import os
+import zipfile
+
+def zip_func(zfile, path):
+    zipname = path + '/' + zfile
+    zip_ref = zipfile.ZipFile(zipname, 'r')
+    zip_ref.extractall(path)
+    zip_ref.close()
+
+path = '/data/prcp/cpc_glb_dly_prec'
+# download using wget should change to python
+os.system('wget -r -nc -np -nd -A zip http://ftp.cpc.ncep.noaa.gov/GIS/GRADS_GIS/GeoTIFF/GLB_DLY_PREC/daily/')
+
+# get list of files
+file_list = os.listdir(path)
+
+# get list of zip
+zip_list = []
+for i in file_list:
+   if i.split('.')[1] == 'zip' and 'latest' not in i:
+       zip_list.append(i)
+
+# unzipping file if not unzipped
+for i in zip_list:
+   filename = path + '/'+ i
+   if os.path.isfile(filename.replace('zip','tif')) == False:
+       zip_func(i, path)
+
+# uploading file_list
+os.system('/scripts/prcp/cpc_glb_dly_prec_upload.py')
---
+++ cpc_glb_dly_prec_upload.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import os
+import psycopg2
+
+path = '/data/prcp/cpc_glb_dly_prec'
+
+file_list = os.listdir(path)
+
+# get list of tif
+tif_list = []
+for i in file_list:
+   if i.split('.')[1] == 'tif' and 'latest' not in i:
+       tif_list.append(i)
+
+
+try:
+    conn = psycopg2.connect("dbname='fato'")
+except:
+    print("I am unable to connect to the database")
+    exit()
+cur = conn.cursor()
+
+cur.execute("select tablename from pg_catalog.pg_tables where schemaname = 'cpc_glb_dly_prec'")
+db_list = cur.fetchall()
+
+db_list_cln = []
+for i in db_list:
+    db_list_cln.append(i[0])
+
+tif_list_db = []
+for i in tif_list:
+   tif_list_db.append(i.lower().split('.')[0])
+
+upload_list = list(set(tif_list_db) - set(db_list_cln))
+
+for i in upload_list:
+    tif_file = path + '/' + i.upper() + '.tif'
+    tif_file = tif_file.replace('FLOAT','float')
+    os.system("raster2pgsql -C -I -N -999 {tif_file} -d cpc_glb_dly_prec.{db_file} | psql fato".format(tif_file=tif_file,db_file=i))
+
