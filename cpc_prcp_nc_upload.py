ef2f485ca6977aa16f89c06a125cb8be193e00d3
ptrierweiler <ptrierweiler@gmail.com>
2018-01-24 00:26:21 +0000

    added grant select

--- cpc_prcp_nc_upload.py
+++ cpc_prcp_nc_upload.py
@@ -62,7 +62,7 @@ for i in range(1,max_band+1):
     name = os.path.join(path,tif_name)
     print(name)
     db_date = datetime.strptime(tif_name.split('_')[2],'%Y%j').strftime('%Y%m%d')
-    table = "cpc_temp_{}".format(db_date)
+    table = "cpc_prcp_{}".format(db_date)
     cur.execute("select count(*) from pg_catalog.pg_tables where "\
                 "schemaname = 'cpc_prcp' and tablename = '{}'".format(table))
     tab_out = cur.fetchall()[0][0]    
@@ -70,20 +70,22 @@ for i in range(1,max_band+1):
         print("Extracting " + name)
         os.system('gdal_translate -b {} NETCDF:"{}":precip {}.tif'.format(i, cpc_file, name))
         print("Splitting " + name)
-        os.system('gdal_translate -srcwin 0 0 72 73 -a_ullr 0 90 180 -90 {out}.tif {out}_east.tif'.format(out=name))
-        os.system('gdal_translate -srcwin 72 0 72 73 -a_ullr -180 90 0 -90 {out}.tif {out}_west.tif'.format(out=name))
+        os.system('gdal_translate -srcwin 0 0 360 360 -a_ullr 0 90 180 -90 -a_ullr 0 90 180 -90 {out}.tif {out}_east.tif'.format(out=name))
+        os.system('gdal_translate -srcwin 360 0 360 360 -a_ullr -180 90 0 -90 {out}.tif {out}_west.tif'.format(out=name))
         print("Merging " + name.replace('.tif','_fix.tif'))
         os.system('gdal_merge.py -o {out}_fix.tif {out}_east.tif {out}_west.tif'.format(out=name))
         print(db_date)
         os.system("gdalwarp -t_srs WGS84 {out}_fix.tif {out}_wgs84.tif".format(out=name))
         os.system("raster2pgsql -I {out}_wgs84.tif -d cpc_prcp.{tab} | psql {db}".format(out=name, tab=table, db=dbname))
         os.remove("{}_east.tif".format(name))
+        # resample down .25
+        cur.execute("update cpc_prcp.{} set rast=ST_Rescale(rast, .25, -.25)".format(table))
+        cur.execute("SELECT AddRasterConstraints('cpc_prcp'::name, '{}'::name, 'rast'::name)".format(table))
+        cur.execute("grant select on cpc_prcp.{} to public".format(table))
         os.remove("{}_west.tif".format(name))
         os.remove("{}.tif".format(name))
         os.remove("{}_wgs84.tif".format(name))
         os.remove("{}_fix.tif".format(name))
         print("{}.tif".format(name))
-        # resample down .25
-        cur.execute("update cpc_prcp.{} set rast=ST_Rescale(rast, .25, -.25)".format(table))
     else:
         print(table + " exists in the db")
